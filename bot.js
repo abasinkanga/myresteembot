7dc5a590de34308946688a8ad8052d1b7c97917d
abasinkanga <36732776+abasinkanga@users.noreply.github.com>
2018-08-01 00:27:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ var RESTEEM_COMMENT = `<center>Congratulations!!!<br>You will be rewarded 1 SMAR
 //var RESTEEM_COMMENT = "Humble apologies for the multiple comments";
 
 var RESTEEM_COMMENT = `<b>Your post has been resteemed. Thank you for using my resteem service</b>
-<b>Join my [discord server](` + URL3 + `) for cool rewards</b>`;
+<b>Get more rewards in my [discord server. Join Here](` + URL3 + `)</b>`;
 
 /////////////
 
